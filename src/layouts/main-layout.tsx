36fb8b25fda69c8a967a501dbc42124463dc0dca
Farid Villacis <faridvl095.fv@gmail.com>
2026-02-22 22:09:44 -0600

    update login

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/common/navbar/navbar";
 const Layout: React.FC = ({ children }: any) => {
   return (
     <div className="min-h-screen bg-gray-100">
-      <Navbar />
+      {/* <Navbar /> */}
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
       </main>
